8d21232c5525abbbcded9d37449ab2df06fbcc1b
ymgt840 <yomo1022@gmail.com>
2023-05-26 14:06:34 +0900

    docker + fastapi (python3.7) + mysq5.7

---
+++ be/api/db.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+host = "sample_db:3308"
+db_name = "sample_db"
+user = "mysqluser"
+password = "mysqlpass"
+
+DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (
+    user,
+    password,
+    host,
+    db_name,
+)
+
+ENGINE = create_engine(
+    DATABASE,
+    echo=True
+)
+
+session = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=ENGINE
+    )
+)
+
+Base = declarative_base()
+Base.query = session.query_property()
